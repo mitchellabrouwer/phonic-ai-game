f5336454c6ff6bc773ffc04697b207c533017445
mitchellabrouwer <mbarron198@gmail.com>
2023-12-02 13:17:42 +1100

    feat: state considerations

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -6,6 +6,14 @@ import LandGrid from "./LandGrid";
 
 export default function Dashboard() {
   const [land, setLand] = useState("1");
+  const [achievedLandLetters, setLandLetters] = useState([]);
+  const [currentLetter, setCurrentLetter] = useState("s");
+  const [timeToComplete, setTimeToComplete] = useState();
+  const [lives, setLives] = useState();
+  const [amountCorrect, setAmountCorrect] = useState();
+  const [difficult, setDifficult] = useState<"easy" | "medium" | "hard">(
+    "easy",
+  );
 
   return (
     <div className="flex h-full flex-col">
